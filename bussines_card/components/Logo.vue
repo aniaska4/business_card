57a28f94c93f23c2d6c0d9d6d1892fa01d53a95c
aniaska4 <aniaska4@wp.pl>
2020-01-02 20:26:15 +0100

    remove folders

--- bussines_card/components/Logo.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="VueToNuxtLogo">
-    <div class="Triangle Triangle--two" />
-    <div class="Triangle Triangle--one" />
-    <div class="Triangle Triangle--three" />
-    <div class="Triangle Triangle--four" />
-  </div>
-</template>
